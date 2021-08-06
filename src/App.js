41e8a367edbff6e2888cfcc07aab2a3540d915f4
unknown <Chikwado>
2021-08-06 22:59:30 +0100

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import Preloader from './Components/Preloader/Preloader'
+import Home from './Containers/Home/Home'
 
 function App() {
+
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect( () => {
+
+    const timer = setTimeout(() => {
+      console.log("Time out done")
+      setLoaded(true)
+    }, 3200);
+    return () => clearTimeout(timer)
+  }, [])
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Preloader />
-      </header>
+        {!loaded?
+        <Preloader /> 
+        :
+        <Home />
+      }
+        
     </div>
   );
 }
---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Home (){
+
+    return (
+        <div className='h-full pt-24 pl-10 w-full'>
+            <div className=' flex-column items-start border w-full justify-start'>
+                <h2 className='text-7xl font-semibold font-san text-left '>
+                    <span className=''>H</span>
+                    <span>i</span>
+                    <span> &#128526;</span>
+                    <span>,</span>
+                    <br />
+                    <span>I</span>
+                    <span>'</span>
+                    <span>m</span>
+                    <span> </span>
+                    <span>C</span>
+                    <span>h</span>
+                    <span>i</span>
+                    <span>k</span>
+                    <span>w</span>
+                    <span>a</span>
+                    <span>d</span>
+                    <span>o</span>
+                    <span>,</span>
+                </h2>
+                <h3 className='text-5xl font-semibold font-san text-left mt-4'>
+                    <span>S</span>
+                    <span>o</span>
+                    <span>f</span>
+                    <span>t</span>
+                    <span>w</span>
+                    <span>a</span>
+                    <span>r</span>
+                    <span>e</span>
+                    <span> </span>
+                    <span>E</span>
+                    <span>n</span>
+                    <span>g</span>
+                    <span>i</span>
+                    <span>n</span>
+                    <span>e</span>
+                    <span>e</span>
+                    <span>r</span>
+                </h3>
+                    
+                <p className='text-base text-left mt-5 mb-5 text-gray-400' >Full stack web developer / A very sound mind</p>
+                <p className='border p-2 w-40 my-2'>Hire me</p>
+            </div>
+        </div>
+         
+    )
+}
+
+export default Home;
