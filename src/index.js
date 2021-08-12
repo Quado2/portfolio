efd3508371151acab628ef326c6e1b9f6de5f503
unknown <Chikwado>
2021-08-12 23:12:02 +0100

    added theming and about to set up redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
