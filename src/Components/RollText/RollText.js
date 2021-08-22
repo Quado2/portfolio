14e3ba514527951239e27544f64fe91ea5d2cfc1
unknown <Chikwado>
2021-08-22 08:11:02 +0100

    made the RollText to appear on visible to screen

--- src/Components/RollText/RollText.js
+++ src/Components/RollText/RollText.js
@@ -1,14 +1,48 @@
+import { useEffect, useState } from 'react'
 import './RollText.scss'
+ 
+function useOnVisible(options) {
+    const [ref, setRef] = useState(false)
+    const [visible, setVisible] = useState(false)
+    useEffect(()=>{
+
+        const observer = new IntersectionObserver(([entry])=>{
+            if(entry.isIntersecting){
+                setVisible(true)
+            }
+        }, options)
+
+        if(ref){
+            observer.observe(ref)
+        }
+
+        return() => {
+            if(ref){
+                observer.unobserve(ref)
+            }
+        }
+
+    },[ref,options])
+
+    return [setRef, visible]
+}
 
 function RollText(props){
-
+    const options = {threshold:1,
+                     rootMargin: '0px 0px -100px 0px'}
+    const [setRef, visible] = useOnVisible(options)
 
 
     return(
-        <div className='roll-text' disabled>
-            {props.text.split('').map((letter,i) => {
+        <div ref={setRef} className='roll-text' disabled>
+            {
+            visible?
+            props.text.split('').map((letter,i) => {
                 return letter===' ' ? <h2 className='text-skin-button' key={i} disabled>&nbsp;</h2> : <h2 key={i} disabled>{letter}</h2>
-            })}
+            })
+            :
+            null
+        }
             
         </div>
     )
