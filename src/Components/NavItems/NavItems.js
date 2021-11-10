d9b692acf634f2bdae8a1bec41a57d6c18aba754
Quado2 <engchikwas404@gmail.com>
2021-11-10 21:00:01 +0100

    animated the nav bar for mobile'

--- src/Components/NavItems/NavItems.js
+++ src/Components/NavItems/NavItems.js
@@ -1,5 +1,6 @@
 import logo from "../../images/logo2.png";
 import './NavItems.css'
+import './NavItems.scss'
 
 import facebook from "../../images/facebook";
 import twitter from "../../images/twitter";
@@ -9,7 +10,7 @@ import github from "../../images/github";
 
 
 
-function NavItems() {
+function NavItems({showNavItems}) {
 	const styles = {
 		top: `py-6 m-0 w-100 flex flex-col items-center justify-center
             bg-skin-logo mb-4`,
@@ -17,20 +18,20 @@ function NavItems() {
 	};
 
 	return (
-		<div className="  NavItems bg-skin-nav h-screen">
-			<div className={styles.top }>
+		<div className="NavItems bg-skin-nav h-screen">
+			<div className={`${styles.top} ${showNavItems ? "smooth-in-logo":""}`}>
 				<img src={logo} alt="logo" className="w-20" />
 				<h4 className="mt-2 text-skin-muted text-l">Chikwado</h4>
 				<h4 className="mt-1 text-skin-muted text-base">Software Engineer</h4>
 			</div>
-			<ul className=" navs text-skin-muted text-lg font-normal flex flex-col items-start">
+			<ul className={`navs text-skin-muted text-lg font-normal flex flex-col items-start ${showNavItems ? "erase animate-in":"" }`}>
                 <li>About</li>
                 <li>Skills</li>
                 <li>Works</li>
                 <li>Education</li>
                 <li>Contact</li>
             </ul>
-            <ul className='socials mt-4 flex w-100 justify-center '>
+            <ul className={`socials mt-4 flex w-100 justify-center ${showNavItems ? " erase animate-in-socials":"" } `}>
                 <li>{github} </li>
                 <li>{linkedin} </li>
                 <li>{twitter} </li>
--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,12 +1,12 @@
 import ThemeSelector from '../ThemeSelector/ThemeSelector'
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu'
 
-export default function Topbar({rotate,menuClicked}){
+export default function Topbar({showNavItems,menuClicked}){
 
     return(
         <div className = 'fixed flex top-4 right-4 z-10'>
-            <ThemeSelector setInvisible={!rotate} />
-            <HamburgerMenu rotate={rotate} menuClicked={menuClicked} />
+            <ThemeSelector setInvisible={!showNavItems} />
+            <HamburgerMenu rotate={showNavItems} menuClicked={menuClicked} />
         </div>
     )
 }
