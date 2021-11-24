e5970710eb008757634ad05ced32e8cf38b9b53e
Quado2 <engchikwas404@gmail.com>
2021-11-24 22:47:58 +0100

    settled the contact form view

--- src/Containers/ContactForm/ContactForm.js
+++ src/Containers/ContactForm/ContactForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import "./ContactForm.scss";
+
 import RollText from "../../Components/RollText/RollText";
 import Input from "../../Components/Input/Input";
 import Backdrop from "../../Components/Backdrop/Backdrop";
@@ -18,7 +18,7 @@ function ContactForm() {
 	}
 
 	return (
-		<div className="contact-form mt-24 flex flex-col items-center w-full">
+		<div className="contact-form mt-20 flex flex-col items-center  w-full">
 			<Backdrop show={submited} />
 			{submited && (
 				<Notification
@@ -27,11 +27,13 @@ function ContactForm() {
 					message="Thanks, your form has been received."
 				/>
 			)}
-			<div className=" mb-10 ml-5 text-left mr-auto text-skin-button text-5xl sm:text-7xl ">
+
+            
+			<div className=" mb-10 ml-5 text-left text-skin-button text-5xl mr-auto sm:text-7xl ">
 				<RollText text="Contact Me" />
 			</div>
-			<div className=' flex items-center justify-center w-full' >
-				<div className=" input-wrapper p-5 rounded border w-11/12">
+			<div className=' flex items-center justify-center w-full ' >
+				<div className=" input-wrapper p-5 rounded border w-11/12 max-w-md">
 					<Input prompt="Enter your name" />
 					<Input prompt=" Enter Your email address" />
 					<Input prompt=" Type your message" inputType="textArea" />
