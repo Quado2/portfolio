1829bc4b1f7884ef46225b64e19ace92c9ffab1e
Quado2 <engchikwas404@gmail.com>
2021-12-05 03:18:39 +0100

    want to check out the mobile view

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -13,28 +13,32 @@ function Project(props) {
 	return (
 		<div
 			ref={setRef}
-			className={`${appearClass} --removeNow project rounded  w-full my-16
+			className={`${appearClass} --removeNow project rounded  w-full my-16 
                 flex ${right ? 'flex-row-reverse': ''} `}
 		>
 
-			<div className=" image my-3 w-7/12 relative">
-				<img className={`h-full hover:filter-none `} src={imageLink} alt={title} />
-                <div className=" layer w-full h-full absolute top-0 left-0 " ></div>
+			<div className=" opacity-5 image flex items-center justify-center absolute h-full w-full my-3 lg:w-7/12 lg:relative  md:w-10/12 ">
+                <div className='relative h-full'>
+                    <img className={`object-cover h-full`} src={imageLink} alt={title} />
+                    <div className=" layer w-full h-full absolute top-0 left-0 bg-blue-400 opacity-10" ></div>
+                </div>
 			</div>
-			<div className={`flex flex-col ${right? ' items-start' : ' items-end ' } justify-center 
-                    top-0 w-5/12  h-full z-10 `}>
-				<div className={`flex flex-col ${right? ' items-start' : ' items-end '} my-3`}>
+			<div className={`flex flex-col h-full z-10 px-4 ${right? ' items-start' : ' items-end ' } justify-center 
+                    lg:w-5/12   `}>
+				<div className={`flex flex-col ${right? ' items-start' : ' items-end '} my-7`}>
 					<h4 className="text-lg">Featured Project</h4>
-					<h3 className="text-3xl text-skin-muted">{title}</h3>
+					<h3 className="text-2xl lg:text-3xl text-skin-muted">{title}</h3>
 				</div>
-				<div className={`desc flex justify-end ${right? ' text-left' : ' text-right' } w-full bg-skin-nav p-7 my-3 rounded`}>
-					<p className='text-lg w-full text-skin-muted'>{description}</p>
+				<div className={`desc flex justify-end ${right? ' text-left' : ' text-right' } 
+                     bg-transparent md:bg-skin-nav  my-4 rounded`}
+                >
+					<p className='text-base md:text-lg w-full text-skin-muted '>{description}</p>
 				</div>
-				<div className={`w-4/5 my-3 justify-end ${right? ' text-left' : ' text-right' } `}>
+				<div className={`w-full md:w-4/5 my-4 justify-end ${right? ' text-left' : ' text-right' } `}>
 					<h3 className='text-base text-skin-muted'>{stacks}</h3>
 				</div>
 
-				<div className={`flex `}>
+				<div className={`flex my-4 `}>
 					<a href={githubLink}>
 						<svg className="github_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" >
 							<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
