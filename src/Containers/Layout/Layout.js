1749006b91c7c85451a66ff2fca1d3e58bbe3644
Quado2 <engchikwas404@gmail.com>
2021-12-19 06:17:43 +0100

    fixed opacity and z-index issues

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -17,7 +17,7 @@ function menuClicked (){
 		<div className="Layout w-full">
 			<div className="flex ">
 				<nav className={`h-screen w-screen fixed top-0 left-0 opacity-0 
-					md:w-36 md:opacity-100 md:z-10 transition-opacity transform md:translate-x-0 ${showNavItems ? "opacity-100 z-10 translate-x-0 ": "opacity-0 -translate-x-full"}`}>
+					md:w-36 md:opacity-100 md:z-10 transition-opacity transform md:translate-x-0 ${showNavItems ? "opacity-100 z-20 translate-x-0 ": "opacity-0 -translate-x-full"}`}>
                    <NavItems setShowNavItems={setShowNavItems} showNavItems={showNavItems} />
                 </nav>
 				<div className='md:ml-36 flex flex-col w-full'>
