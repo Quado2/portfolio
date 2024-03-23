0950fe3aa99646d0483497f599c8459899301b61
Quado2 <engchikwas404@gmail.com>
2024-03-23 10:29:43 +0100

    update changes to portfolio

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,4 +1,3 @@
-
 import RollText from "../../Components/RollText/RollText";
 import Bounce from "../../Components/Bounce/Bounce";
 
@@ -6,50 +5,66 @@ import useOnVisible from "../../hooks/useOnVisible";
 import { useEffect } from "react";
 
 function About() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-	useEffect(()=>{window.scrollTo(0, 0)},[])
+  const options = {
+    rootMargin: "0px 0px -200px 0px",
+  };
+  const [setRef, appearClass] = useOnVisible(options);
 
-	const options = {
-		rootMargin: '0px 0px -200px 0px'
-	   }
-	const [setRef, appearClass] = useOnVisible(options)
-	
+  return (
+    <div className="about m-0 mt-20 ">
+      <div className="about-top text-skin-button text-5xl md:text-7xl mx-5">
+        <RollText text="About Me" />
+        <div
+          ref={setRef}
+          className={`${appearClass} --removeNow w-full about-level2 mt-5 flex flex-wrap items-start justify-start `}
+        >
+          <div className={`w-full md:w-1/2 `}>
+            <h4 className="about-description text-base md:text-lg text-skin-muted pr-5 text-justify">
+              I am is a skilled Full-Stack Engineer with 5+ years of experience
+              in building efficient, scalable software across diverse domains. I
+              have led various projects in the fields of medicine, government,
+              eCommerce, Web3 finance, and education, using microservices and
+              technologies like RabbitMQ, Kafka, Amazon Web Services (AWS) such
+              as AWS Lambda, AWS SQS and SNS, Api Gateways and many others. I’m
+              also proficient in designing software architectures applying
+              relevant software design patterns and leveraging technologies like
+              Java, Spring Boot, Ruby on Rails (Rails), Node.js, GraphQL,
+              TypeScript, React.js, and Next.js. <br/> <br/>
+							On top of this,  I am an
+              excellent communicator with strong English language skills, both
+              spoken and written. I can effectively iterate with corporate
+              executives to add definitions to technical requirements and also
+              convey ideas to same without a technical background. This ability
+              ensures clear and concise communication, facilitating
+              collaboration and understanding between technical and
+              non-technical stakeholders.
+            </h4>
 
-	return (
-		<div className="about m-0 mt-20 ">
-			<div className="about-top text-skin-button text-5xl md:text-7xl mx-5">
-				<RollText text="About Me" />
-				<div ref={setRef} className={`${appearClass} --removeNow w-full about-level2 mt-5 flex flex-wrap items-start justify-start `}>
-					<div className={`w-full md:w-1/2 `}>
-						<h4 className="about-description text-base md:text-lg text-skin-muted pr-5 text-left">
-							Hello! I’m Chikwado Emmanuel. I'm extremely passionate about software
-							development, I love the challenges that comes with it and the exploding
-							joy that comes after you've nailed it. I have skills in lots of front end
-							tools as well as backend. I learn very quickly, goal driven and prefers
-							to work as a team, albeit confident to deliver works personally.
-						</h4>
-
-						<div className="about-contact text-left mt-4 ">
-							<h3 className="text-2xl">Contact</h3>
-							<div className="text-base text-skin-muted flex flex-col pt-3 items-start">
-								<div className="flex">
-									<h3 className="w-24 text-left">Phone:</h3>
-									<h3>+2348137796116</h3>
-								</div>
-								<div className="flex">
-									<span className=" w-24 text-left">Email:</span>
-									<span>chikwadonuel@gmail.com</span>
-								</div>
-							</div>
-						</div>
-					</div>
-                    <div className={`w-full md:w-1/2`}>
-                        <Bounce />
-                    </div>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="about-contact text-left mt-4 ">
+              <h3 className="text-2xl">Contact</h3>
+              <div className="text-base text-skin-muted flex flex-col pt-3 items-start">
+                <div className="flex">
+                  <h3 className="w-24 text-left">Phone:</h3>
+                  <h3>+2348137796116</h3>
+                </div>
+                <div className="flex">
+                  <span className=" w-24 text-left">Email:</span>
+                  <span>chikwadonuel@gmail.com</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`w-full md:w-1/2`}>
+            <Bounce />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/Containers/Experience/Experience.js
+++ src/Containers/Experience/Experience.js
@@ -2,6 +2,85 @@ import RollText from "../../Components/RollText/RollText";
 import Work from "../../Components/Work/Work";
 import { useEffect } from "react";
 
+const experiences = [
+  {
+    role: "Senior Software Engineer",
+    company: "Outsource Global Technologies Limited",
+    period: "June 2022 - Present (Full Time)",
+    jd: `•	Operating as a senior software engineer at Africa’s leading business and knowledge process outsourcing company, serving clients in the US, UK and Japan.;
+    •	Worked collaboratively with five different teams on five distinct products, each comprising two front-end developers, two back-end developers, and one UI/UX designer;
+    •	Designed and developed reusable UI components using standard front-end design patterns;
+    •	Wrote clean, efficient, reusable, fast, non-blocking, and testable web sockets for real-time communications;
+    •	Increased the rate of feature delivery by 200% within the first month, which led to a promotion to the role of a senior developer after just two months as a mid-level developer;
+    •	Utilized AWS Lambda, API gateways, Amazon Simple Queue Service (Amazon SQS), and Amazon Simple Notification Service (Amazon SNS) to implement an event-driven architecture for backlog payments;
+    •	Employed Docker and Kubernetes for containerization;
+    •	Practiced test-driven development (TDD) for unit and integration testing using Jest and behavior-driven development (BDD) for end-to-end testing with Puppeteer and Cucumber;
+    •	Collaborated with stakeholders and the software development team to refine project requirements;
+    •	Participated in code reviews, providing constructive feedback to enhance code quality and maintainability
+    `,
+  },
+  {
+    role: "Senior Software Engineer",
+    company: "Molcom Multi-Concepts Limited, Abuja, Nigeria.",
+    period: "June 2022 - Present(Part Time)",
+    jd: `• Currently operating as a senior software developer, leading a team
+    comprising one UI/UX designer, two front-end developers, and two backend developers.;
+    • Designed and architected software for a numbering management system
+    used by the Nigerian Communications Commission (NCC) for administering
+    number blocks in Nigeria.;
+    • Designed flow algorithms for various product features.;
+    Developed APIs using Spring Boot;
+    • Built UI components using React.js and integrated them with APIs developed.;
+    for the back-end using Spring Boot.;
+    • Managed collaborations and versioning with git and github.;
+    • Implemented highly efficient and testable web socket for real time.;
+    notifications using spring boot web socket and socket.io.;
+    • Authored unit, integration, and end-to-end tests for the application using
+    tools like Jest and Cypress.;
+    • Managed deployments to tomcat.;
+    `,
+  },
+  {
+    role: "Full Stack Developer",
+    company: "BizGem, VI, Lagos, Nigeria.",
+    period: "Dec 2021 - Jun 2022 (Full Time)",
+    jd: `
+    • Took charge of the design and development of reusable React front-end UI
+    components.;
+    •C reated clean, high-performance APIs and GraphQL endpoints in Next js
+    framework and TypeScript.;
+    • Designed and implemented database schemas for SQL and NoSQL
+    databases using Prisma object-relational mapping (ORM) tool.;
+    • Integrated UI with REST APIs and GraphQL back ends.;
+    • Maintained a legacy codebase based on Backbone and Marionette.js.;
+    • Authored end-to-end tests using Cypress.;
+    ;
+    `,
+  },
+  {
+    role: "Full Stack Developer",
+    company: "Paradiso Technologies Limited.",
+    period: "Mar 2019 - August 2021 (Full Time)",
+    jd: `• Engaged in full-stack development in various projects for different clients.;
+    • Developed customized applications for a diverse range of clients using
+    Node.js, Spring Boot, React.js, and Next.js.;
+    • Employed Node.js for server-side development to achieve optimal
+    performance and responsiveness.;
+    • Designed and implemented scalable, maintainable APIs using Spring Boot,
+    enhancing communication between front-end and back-end systems
+    Implemented responsive and user-friendly interfaces using React.js and
+    Next.js, tailored to meet specific client design criteria.;
+    • Applied Agile/Scrum methodologies for delivering software solutions in
+    iterative and incremental cycles, focusing on client satisfaction.;
+    • Conducted thorough code reviews.;
+    • Worked with cross-functional teams to maintain code quality and meet
+    client expectations.;
+    • Collaborated with clients to identify requirements, analyze business needs,
+    and devise effective technical solutions.;
+    `,
+  },
+];
+
 function Experience() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -13,38 +92,15 @@ function Experience() {
         <RollText text="Experience" />
       </div>
       <div className="pb-4 h-full">
-        <Work
-          role="Full Stack Developer"
-          company="Quickgem digital solutions"
-          period="December 2021 - Present"
-          jd="
-                      Building and maintaining loan application using the MVVC architecture.;
-                      Providing post production support to users on application built.;
-                      Paired with the team to develop in-house core banking application using agile methodology."
-        />
-        <Work
-          role="Frontend Developer"
-          company="Hotels.ng"
-          period="August 2021 - Oct 2021"
-          jd="Led a front-end sub team to build and integrate Google drive plugin tool.;
-                      Managed Real time communication in the channel using Centrifuge library.;
-                      Migrated the application from a native react application to a Single-SPA micro frontend.;
-                      Completed the internship program as one of the 300 finalists out of 12,000 participants that started."
-        />
-        <Work
-          role="Full Stack Developer"
-          company="Paradiso Technologies Limited"
-          period="July 2019 - present"
-          jd="Built the company’s web application where potential clients can view and make requests for the products and services.;
-                      Developed the administration dashboard where the contents can be managed.;
-                      Maintained, improved and extended the overall functionalities of the application to suit current needs."
-        />
-        <Work
-          role="Technical Assitant and Developer"
-          company="ICT unit, FUTO"
-          period="May 2018 - March 2019"
-          jd="Asiisted the lead programmer with software maintenance and upgrade. Trained colleagues on soft development stacks"
-        />
+        {experiences?.map((exp) => (
+          <Work
+            role={exp.role}
+            company={exp.company}
+            period={exp.period}
+            jd={exp.jd}
+          />
+        ))}
+
       </div>
     </div>
   );
